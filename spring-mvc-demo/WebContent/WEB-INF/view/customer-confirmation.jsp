1c9c3ac9ca119881e417e71b8b1b46710a5d580e
Fabio Martins <fabiomartins1988@gmail.com>
2019-07-19 09:32:23 -0300

    Validação de formularios e validações personalizadas

--- spring-mvc-demo/WebContent/WEB-INF/view/customer-confirmation.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/customer-confirmation.jsp
@@ -8,6 +8,11 @@
 <body>
 	
 	The customer is confirmed: ${customer.firstName} ${customer.lastName}
-	
+	<br><br>
+	Free Passes: ${customer.freePasses}
+	<br><br>
+	Postal Code: ${customer.postalCode}
+	<br><br>
+	Course Code: ${customer.courseCode}
 </body>
 </html>
--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/CustomerController.java
@@ -41,6 +41,11 @@ public class CustomerController {
 	public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult bindingResult) {
 		
 		System.out.println("Last Name: " + theCustomer.getLastName() );
+		
+		System.out.println("Binding result: " +bindingResult);
+		
+		System.out.println("\n\n\n\n");
+		
 		if (bindingResult.hasErrors()) {
 			return "customer-form";
 		} else {
