757f2d8116222937a8a2ab1157520dd3770f8b0d
Fabio Martins <fabiomartins1988@gmail.com>
2019-07-23 11:11:15 -0300

    CRUD(Basico) CAP-22

---
+++ hibernate-tutorial/src/com/luv2code/hibernate/demo/DeleteStudentDemo.java
@@ -0,0 +1,48 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class DeleteStudentDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+			int studentId = 4;
+			
+			// now get anew session and start transaction
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+			System.out.println("Delete student get id: " + studentId);
+			Student myStudent = session.get(Student.class, studentId);
+			
+			// delete the student
+			System.out.println("Delete student...");
+			session.delete(myStudent);
+			
+			
+
+			System.out.println("Delete student id=2");
+			session.createQuery("delete from Student where id=2").executeUpdate();
+			// commit the transaction
+			session.getTransaction().commit();
+
+			System.out.println("Done!");
+
+		} finally {
+			factory.close();
+		}
+	}
+
+}
